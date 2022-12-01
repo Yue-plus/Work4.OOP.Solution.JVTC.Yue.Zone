0fe42162759f522cba0d929562efdeb95734cf4e
Yue-plus <Yue_plus@foxmail.com>
2022-12-01 20:09:31 +0800

    新建项目

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module zone.yue.jvtc.solution.oop.work4.work4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens zone.yue.jvtc.solution.oop.work4.work4 to javafx.fxml;
+    exports zone.yue.jvtc.solution.oop.work4.work4;
+}
---
+++ src/main/java/zone/yue/jvtc/solution/oop/work4/work4/HelloController.java
@@ -0,0 +1,14 @@
+package zone.yue.jvtc.solution.oop.work4.work4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
