ecb2605b5af21e661d985e286237efe854bdd81b
Yue-plus <Yue_plus@foxmail.com>
2022-12-02 00:01:00 +0800

    一点细节

--- src/main/java/zone/yue/jvtc/solution/oop/work/UserRegistrationController.java
+++ src/main/java/zone/yue/jvtc/solution/oop/work/UserRegistrationController.java
@@ -1,16 +1,6 @@
 package zone.yue.jvtc.solution.oop.work;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-
 /**
  * @author Yue_plus
  */
-public class UserRegistrationController {
-    @FXML
-    Label hobby;
-
-    @FXML
-    ChoiceBox local;
-}
+public class UserRegistrationController {}
